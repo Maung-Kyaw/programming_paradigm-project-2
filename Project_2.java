6134ca978753c22031c9d25185628acda1fdc779
Kyaw Zin  Thant <68962256+Maung-Kyaw@users.noreply.github.com>
2025-03-07 19:23:53 +0700

    Add files via upload

---
+++ Project_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project2;
+
+/**
+ *
+ * @author kyawz
+ */
+public class Project_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
