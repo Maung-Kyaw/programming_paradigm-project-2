8cdf2f23ebb0eff6e5156ab979f502d6125836b9
Trinnaya <142000548+Trinnaya@users.noreply.github.com>
2025-03-08 15:26:02 +0700

    Update Project_2.java

--- Project_2.java
+++ Project_2.java
@@ -4,11 +4,11 @@ Kyaw Zin Thant 6581178
 Chartwut Piriyapanyaporn 6481227
 Phurilap Kitlertpaisan 6680251*/
 
-package Project2;
+package Project2_6581147;
 
 import java.io.*;
 import java.util.*;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.*;
 
 class Place {
     private final String name;
@@ -97,14 +97,15 @@ class AgencyThread extends Thread {
     }
 
     public void run() {
-        for (int day = 1; day <= Project_2.days; day++) {
+        for (int day = 1; day <= Project2_6581147.days; day++) {
             try {
                 dayCounter.waitForDay(day); 
 
                 int arrivals = random.nextInt(maxDailyArrival + 1);
                 remainingCustomers += arrivals;
-                System.out.println(Thread.currentThread().getName() + " >> new arrival = " + arrivals + " remaining customers = " + remainingCustomers);
-
+                System.out.printf("%22s >> new arrival = %2d %39s = %3d\n", Thread.currentThread().getName(), 
+                        arrivals, "remaining customers", remainingCustomers);
+                
                 arrivalBarrier.await();
 
                 if (remainingCustomers > 0) {
@@ -118,12 +119,19 @@ class AgencyThread extends Thread {
                         }
                         if (tour.addCustomers(send)) {
                             remainingCustomers -= send;
-                            System.out.println(Thread.currentThread().getName() + " >> send " + send + " customers to " + tour.getName() + " seats taken = " + tour.getCurrentCustomers());
+                            System.out.printf("%22s >> send %2d customers to %-15s seats taken = %2d\n", Thread.currentThread().getName(), send, tour.getName(), tour.getCurrentCustomers());
                         }
+                        
                     }
                 }
 
-                barrier.await();
+                
+                try{
+                    barrier.await();
+                }
+                catch(InterruptedException | BrokenBarrierException e ){
+                    System.err.println(e.getClass().getName());
+                }
             } catch (Exception e) {
                 System.err.println(e.getClass().getName());
             }
@@ -147,21 +155,30 @@ class OperatorThread extends Thread {
     }
 
     public void run() {
-        for (int day = 1; day <= Project_2.days; day++) {
+        for (int day = 1; day <= Project2_6581147.days; day++) {
             try {
                 dayCounter.waitForDay(day);
                 
-                barrier.await();
+                try{
+                    barrier.await();
+                }
+                catch(InterruptedException | BrokenBarrierException e ){
+                    System.err.println(e.getClass().getName());
+                }
 
                 Place place = places.get(random.nextInt(places.size()));
                 synchronized(place){
                     int customers = tour.takeCustomers();
                     if (customers > 0) {
                         place.addVisitors(customers);
-                        System.out.println(Thread.currentThread().getName() + " >> take " + customers + " customers to " + place.getName() + " visitor count = " + place.getVisitorCount());
+                        System.out.printf("%22s >> take %2d customers to %-15s visitor count = %3d\n", 
+                                Thread.currentThread().getName(), 
+                                customers, place.getName(), 
+                                place.getVisitorCount());
                     } else {
-                        System.out.println(Thread.currentThread().getName() + " >> no customer");
+                        System.out.printf("%22s >> no customer\n", Thread.currentThread().getName());
                     }
+                    //System.out.printf("%22s >>\n",Thread.currentThread().getName());
                 }
                 
             } catch (Exception e) {
@@ -171,18 +188,17 @@ class OperatorThread extends Thread {
     }
 }
 
-public class Project_2 {
+public class Project2_6581147 {
     public static int days = 0;
     
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("New file name: ");
-        String fileName = input.nextLine();
-        Scanner scan = null;
+        Scanner input= new Scanner(System.in);
+        String fileName="config_1.txt";
+        Scanner scan=null;
         
         while(scan==null){
             try {
-                scan = new Scanner(new File("src/main/java/Project2/"+fileName));
+                scan = new Scanner(new File("src/main/java/Project2_6581147/"+fileName));
 
                 int agencyCount = 0, maxArrival = 0, tourCount = 0, tourCapacity = 0, placeCount = 0;
 
@@ -208,12 +224,16 @@ public class Project_2 {
                     }
                 }
 
-                List<Tour> tours = new ArrayList<>();
+                System.out.printf("%22s >> ================ Parameters ================\n\n",Thread.currentThread().getName());
+                System.out.printf("%22s >> %-18s = %d\n", Thread.currentThread().getName() ,"Days of simulation", days);
+                System.out.printf("%22s >> %-18s = %d\n", Thread.currentThread().getName(), "Max arrival", maxArrival);
+                
+                ArrayList<Tour> tours = new ArrayList<>();
                 for (int i = 0; i < tourCount; i++) {
                     tours.add(new Tour("Tour_" + i, tourCapacity));
                 }
 
-                List<Place> places = new ArrayList<>();
+                ArrayList<Place> places = new ArrayList<>();
                 for (int i = 0; i < placeCount; i++) {
                     places.add(new Place("Place_" + i));
                 }
@@ -222,22 +242,41 @@ public class Project_2 {
                 CyclicBarrier arrivalBarrier = new CyclicBarrier(agencyCount);
                 CyclicBarrier sendBarrier = new CyclicBarrier(agencyCount + tourCount);
 
-                List<AgencyThread> agencies = new ArrayList<>();
+                ArrayList<AgencyThread> agencies = new ArrayList<>();
                 for (int i = 0; i < agencyCount; i++) {
                     AgencyThread agency = new AgencyThread("AgencyThread_" + i, tours, maxArrival, dayCounter, arrivalBarrier, sendBarrier);
                     agencies.add(agency);
                     agency.start();
                 }
+                System.out.printf("%22s >> %-18s = [",Thread.currentThread().getName(), "AgencyThread");
+                for (AgencyThread agency : agencies) {
+                    System.out.printf(" %s ", agency.getName());
+                }
+                System.out.printf("]\n");
+                
+                System.out.printf("%22s >> %-18s = %d\n", Thread.currentThread().getName(), "Tour capacity", tourCapacity);
+                System.out.printf("%22s >> %-18s = [",Thread.currentThread().getName(), "OperatorThread");
 
-                List<OperatorThread> operators = new ArrayList<>();
+                ArrayList<OperatorThread> operators = new ArrayList<>();
                 for (Tour tour : tours) {
                     OperatorThread operator = new OperatorThread(tour, places, dayCounter, sendBarrier);
                     operators.add(operator);
                     operator.start();
+                    System.out.printf(" %s ", tour.getName());
                 }
+                System.out.printf("]\n");
+                
+                System.out.printf("%22s >> %-18s = [",Thread.currentThread().getName(), "Places");
+                for (Place place : places) {
+                    System.out.printf(" %s ",place.getName());
+                }
+                System.out.printf("]\n");
 
                 for (int day = 1; day <= days; day++) {
-                    System.out.println(Thread.currentThread().getName() + " >> Day " + day);
+                    System.out.println();
+                    System.out.printf("%22s >> ============================================\n",Thread.currentThread().getName());
+                    System.out.printf("%22s >> Day %d\n",Thread.currentThread().getName(),day);
+                    System.out.println();
                     dayCounter.incrementDay(); 
                     Thread.sleep(100); 
                 }
@@ -248,11 +287,15 @@ public class Project_2 {
                 for (OperatorThread operator : operators) {
                     operator.join();
                 }
-
-                System.out.println(Thread.currentThread().getName() + " >> Summary");
+                System.out.println();
+                System.out.printf("%22s >> ============================================\n",Thread.currentThread().getName());
+                System.out.println();
+                
+                System.out.printf("%22s >> Summary\n",Thread.currentThread().getName());
                 tours.sort(Comparator.comparing(Tour::getTotalCustomers).reversed().thenComparing(Tour::getName));
                 for (Tour tour : tours) {
-                    System.out.println(Thread.currentThread().getName() + " >> " + tour.getName() + "   total customers= " + tour.getTotalCustomers());
+                    System.out.printf("%22s >> %-11s total customers= %3d\n", Thread.currentThread().getName(), 
+                            tour.getName(), tour.getTotalCustomers());
                 }
             
                 scan.close();
@@ -264,7 +307,7 @@ public class Project_2 {
                 fileName=input.next();
             } catch (IOException | InterruptedException e) {
                 System.err.println(e.getClass().getName());
-                }
+            }
         
         }
 
